4b9d1a912e51d42660f2144797dfa8bf5be459e4
Danil Domnich <domni4.d2@yandex.ru>
2024-10-01 12:19:20 +0300

    Добавлен файл лабораторной работы 1.2

---
+++ laboratory-work-1.2/laboratory-work-1.2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a, b, c;
+    cout << "¬ведите a: ";
+    cin >> a;
+    cout << "¬ведите b: ";
+    cin >> b;
+    cout << "¬ведите c: ";
+    cin >> c;
+    if ((a + b > c) && (a + c > b) && (b + c > a)) {
+        cout << "“реугольник существует";
+    }
+    else {
+        cout << "“реугольник не существует";
+    }
+}
